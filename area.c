b36ea2224119eb109ff2cb8173c8637615e6517a
Amrita MS <55478795+amrita99@users.noreply.github.com>
2021-11-23 08:29:24 -0500

    Create area.c

---
+++ area.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{   float area,length,breadth;
+    printf("Enter the length ");
+    scanf("%f/n",&length);
+    printf("Enter the breadth ");
+    scanf("%f/n",&breadth);
+    area=length*breadth;
+    
+    printf("area = %f cm",area);
+
+    return 0;
+}
