c04865d649f0c469a4c55babc13f439f1370548c
Amrita MS <55478795+amrita99@users.noreply.github.com>
2021-11-23 13:17:19 -0500

    Create conversion.c

---
+++ conversion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{   float lm,num;
+    printf("Enter a number");
+    scanf("%f",&num);
+    lm=num*100 ;//1 m=100 cm
+    printf("The result is : %f",lm);
+    
+    
+    return 0;
+    
+
+
+    
+}
