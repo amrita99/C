1afda3d887fb8d7c14f24c13c84550667052d1e2
Amrita MS <55478795+amrita99@users.noreply.github.com>
2021-11-23 13:52:28 -0500

    Create checking.c

---
+++ checking.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{   
+    int num,rem;
+    printf("enter a number");
+    scanf("%d",&num);
+    rem=num%2;
+    if(rem==0)
+      printf("The number is even");
+    else
+      printf("The number is odd");
+      
+      
+    return 0;
+
+    
+    
+  
+    
+
+
+    
+}
