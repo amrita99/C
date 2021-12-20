3dfd4ccc0ba670200b7c236514cbbc12063a5094
Amrita MS <55478795+amrita99@users.noreply.github.com>
2021-12-20 12:44:31 -0500

    Create declaration and initialization of array.c

---
+++ declaration and initialization of array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(){
+    int i=0;
+    int marks[5]={23,56,90,67,88};//initializing and declaration of array
+   
+    for(i=0;i<5;i++){
+        printf("%d\n",marks[i]);
+    }
+    return 0;
+    
+}
