da3d7060e90423784e688a1c339196ed6e06a7c8
Amrita MS <55478795+amrita99@users.noreply.github.com>
2021-12-21 00:18:33 -0500

    Create getarray.c

---
+++ getarray.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void getarray(int arr[]){
+    printf("Elements of array are:");
+    for(int i=0;i<5;i++){
+        printf("%d",arr[i]);}
+    }
+    int main(){
+        int arr[5]={3, 4, 5, 6, 7 };
+        getarray(arr);
+        return 0;
+       
+    }
+    
+
+
+    
+
